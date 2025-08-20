e60f4fa955e14f13e79ff01db95c041f77b41681
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2025-08-20 14:27:05 -0300

    indo
    ''

--- src/Components/FilmeHeader.js
+++ src/Components/FilmeHeader.js
@@ -1,50 +1,32 @@
 import './FilmeHeader.css'
 import { TbNumber12Small } from "react-icons/tb";
 
-
 function FilmeHeader() {
-    
-    return(
-        
-    <div className='PaiHeader'>
-            <div className='NomeFilme'> Luta pela Fé - A História do Padre Stu</div>
-           
+    return (
+        <div className='PaiHeader'>
+            <div className='NomeFilme'>Luta pela Fé - A História do Padre Stu</div>
 
-<div className='infoFilme'>
-        
-        <div className='infos'>
+            <div className='infoFilme'>
 
-             <div className='Tempo'>2022 2h 4min</div>
-            
-                 <div className='classificacao'> 
+                <div className='Tempo'>2022 2h 4min</div>
+
+                <div className='classificacao'> 
                     <TbNumber12Small className="idade" />
-                    <div className='sobre'> violência, drogas lícitas e linguagem imprópria</div> <div/>    
-                 </div>
-
-            <div className='descricao'>
-                <div className='texto'> Este drama de 2022 sobre a jornada de um homem até o sacerdócio se baseia em uma história real.</div>
-            </div>
-
-
-        </div>
-            <div className='genero-elenco-estefilme'>
-                
-                <div className='elenco'>
-                    <div className='nome'> Mark Wahlberg </div>
-                    <div className='nome'>Mel Gibson</div>
-                    <div className='nome'>Jacki Weaver</div>
-                    <div className='nome'>Teresa Ruiz</div>
-                    <div className='nome'>Malcolm McDowel</div>
-                    <div className='nome'>Annet Mahendru</div>
-                    <div className='nome'>Cody Fern</div>
+                    <div className='sobre'>violência, drogas lícitas e linguagem imprópria</div>
                 </div>
 
+                <div className='descricao'>
+                    <div className='texto'>
+                        Este drama de 2022 sobre a jornada de um homem até o sacerdócio se baseia em uma história real.
+                    </div>
+                </div>
+
+             
+              
+
             </div>
-
-    </div>
-</div>
+        </div>
     )
-
 }
 
-export default FilmeHeader
+export default FilmeHeader;
--- src/pages/FilmePage.js
+++ src/pages/FilmePage.js
@@ -1,13 +1,14 @@
 import FilmeBanner from "../Components/FilmeBanner";
 import "./FilmePage.css";
 import Header from '../Components/FilmeHeader'
-
+import { FaPlay } from "react-icons/fa";
+import { CiCircleInfo } from "react-icons/ci";
 
 function FilmePage() {
     return (
         <div className="PaidetodosFilme">
             <div className="FilmeBanner">
-                <FilmeBanner />
+              
             </div>
             <div className="infos">
              
